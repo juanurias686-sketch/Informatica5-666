dfae0c2a3713f842fa1994a08cd7aa4cb56144b9
juanurias686-sketch <juanurias686@gmail.com>
2026-08-13 15:43:55 +0000

    {kjgñkfgb

--- story.py
+++ story.py
@@ -14,15 +14,21 @@ def main():
     # print("Hello", end=" ")
     # print(planet)
 
-    name = input("What is your name? ")
-    color = input("Tell me a color: ")
-    adjetive = input("say an adjetive: ")
-    goal = input("A goal you would like to achieve: ")
+    name = input("What is your name? ").strip().title()
+    color = input("Tell me a color: ").strip().
+    adjetive = input("say an adjetive: ").strip()
+    goal = input("A goal you would like to achieve: ").strip().
 
     print(f"Hello, {name}!", end="\n\n")
 
     print("This is yor story:")
+    print(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today I will finally {goal}.")
+    print(f"At dawn the sky turned {color}, and the air felt {adjetive}. I decided today I will finally {goal}.".upper())
 
 
-if __name__=="_main_":
+
+
+
+
+if __name__=="__main__":
     main()
