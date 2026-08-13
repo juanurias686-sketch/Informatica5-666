352def574d418b4cd3887403cca580c8854b5924
juanurias686-sketch <juanurias686@gmail.com>
2026-08-13 14:59:32 +0000

    2

---
+++ story.py
@@ -0,0 +1,28 @@
+def main():
+    # planet = input("Planet:")
+
+    # # Separation
+    # print("Hello", planet)
+
+    # # Concatenation
+    # print("Hello " + planet)
+
+    # #Formated Strings
+    # print(f"Hello {planet}")
+
+    # # Ending
+    # print("Hello", end=" ")
+    # print(planet)
+
+    name = input("What is your name? ")
+    color = input("Tell me a color: ")
+    adjetive = input("say an adjetive: ")
+    goal = input("A goal you would like to achieve: ")
+
+    print(f"Hello, {name}!", end="\n\n")
+
+    print("This is yor story:")
+
+
+if __name__=="_main_":
+    main()
