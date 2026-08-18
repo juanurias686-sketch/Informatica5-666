0c21c5631846f43b049b60bb2b3e28cb1b0c8ed3
juanurias686-sketch <juanurias686@gmail.com>
2026-08-18 20:19:03 +0000

    opuhrg

---
+++ Currency.py
@@ -0,0 +1,6 @@
+def main():
+    col=float(input("What do you have left in pesos?"))
+    sol=float(input("What do you have left in soles?"))
+    reais=float(input("What do you have left in reais?"))
+
+    usd=
