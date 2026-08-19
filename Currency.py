d6fbcde00ac463615b62d0dd2067d5b94a16fc5b
juanurias686-sketch <juanurias686@gmail.com>
2026-08-19 18:01:00 +0000

    update variable

--- Currency.py
+++ Currency.py
@@ -3,4 +3,11 @@ def main():
     sol=float(input("What do you have left in soles?"))
     reais=float(input("What do you have left in reais?"))
 
-    usd=
+    usd = (col * 0.0032) + (sol * 0.30) + (reais * 0.19)
+    mxn = round(usd * 17.07, 2)
+
+    print(f"USD: {round(usd , 2)}")
+    print(f"MXN: {mxn}")
+
+if __name__=="__main__":
+    main()
---
+++ update-variable.py
@@ -0,0 +1,39 @@
+def main():
+    # Aumented Assignment Operator
+    score=10
+    score += 10
+    print(score)
+
+    # Substraction Assignment Operator
+    #x = x - y
+    #x-=Y
+    grade = 100
+    grade -= 15
+
+    print(grade)
+
+    # Multiplication Assignment Operator
+
+    balance = 10
+    balance *= 4
+    print(balance)
+
+    # Division Assignment Operator
+
+    a = 10
+    x /= 5
+
+    print(x)
+
+    #Modulus Assignment Operator
+
+    a = 10
+    b = 4
+    a%= b
+    print(a)
+
+
+
+if __name__=="__main__":
+    main()
+
