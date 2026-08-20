bad79429f917d5e70b98c9eeee75207c1ca98586
juanurias686-sketch <juanurias686@gmail.com>
2026-08-20 14:48:08 +0000

    phdfouh

---
+++ moores.py
@@ -0,0 +1,9 @@
+def main():
+    transistors=17.8
+    years=float(input("How many years do you wanna increase?: "))
+    transistors *= years*2/2
+    print(transistors, "Billion")
+if __name__=="__main__":
+    main()
+
+
