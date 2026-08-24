54ce14bd008452049b4623cfe4949e341ce3d30c
juanurias686-sketch <juanurias686@gmail.com>
2026-08-24 15:00:37 +0000

    password

---
+++ password.py
@@ -0,0 +1,22 @@
+def main():
+
+        password = int("4321")
+
+        attempt = int(input("Enter password: "))
+        if attempt == password:
+                print("Correct password")
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
