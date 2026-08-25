87fc313944aaf2900c046a8748e7359d692e5c98
juanurias686-sketch <juanurias686@gmail.com>
2026-08-25 20:20:24 +0000

    kufiutf

---
+++ rating.py
@@ -0,0 +1,22 @@
+def main():
+    print("Carniceria Chapala")
+
+    rating = float(input("Rate Carniceria Chapala: "))
+
+    if rating = 5:
+        print("Perfection")
+    elif rating >= 4.5:
+        print("Perfection")
+    elif rating >= 4:
+        print("Excelent")
+    elif rating >= 3:
+        print("Good")
+    elif rating >= 2:
+        print("fair")
+    else:
+        print("Poor")
+
+
+
+if __name__=="__main__":
+    main()
---
+++ worldcup.py
@@ -0,0 +1,21 @@
+def main():
+    spain = int(input("Spain goals: "))
+    argentina = int(input("Argentina goals: "))
+
+    if spain > argentina:
+        print("Spain is the winnner!")
+    elif argentina > spain:
+        print("Argentina is the winner!")
+    else:
+        print("Is a tie.")
+
+    print("gg")
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
