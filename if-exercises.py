ff71186215dfdfbc54ff9f5ffc0db1abcc17bf40
juanurias686-sketch <juanurias686@gmail.com>
2026-08-26 18:11:31 +0000

    satanic calc

---
+++ if-exercises.py
@@ -0,0 +1,48 @@
+def main():
+#easy
+
+    #intg = int(input("Type an integer number: "))
+
+
+   # if intg < 0:
+        #print(intg * - 1)
+
+    #else:
+        #print(intg)
+
+
+
+    #Medium
+
+    #num1 = float(input("Enter the first number: "))
+
+   # num2 = float(input("Enter the second number: "))
+
+  #  op = input("Enter an operation: ")
+
+
+
+  #  if op == "add":
+     #   print(num1 + num2)
+
+   # elif op == "sub":
+    #    print(num1 - num2)
+
+  #  elif op == "mult":
+      #  print(num1 * num2)
+
+
+
+    #Hard
+
+
+    
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
