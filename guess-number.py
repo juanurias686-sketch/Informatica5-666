f76fbffc011af760506b79440159768e852c4cb1
juanurias686-sketch <juanurias686@gmail.com>
2026-09-03 15:48:44 +0000

    guess number 666

--- guess-number.py
+++ guess-number.py
@@ -3,18 +3,58 @@ def main():
 
 
 
-
-
         name = input("Whats your name?: ")
-        guess = float(input (f"Well,{name}, I am thinking of a number between 1 and 100 Take a guess: "))
+        dif = input("Select a dificult, Easy, Medium or Hard: ").title().strip()
+        if dif == "Easy":
+            print(f"Well,{name}, I am thinking of a number between 1 and 100")
+
+
+
         number = random.randint(1,100)
+        guess = int(input("Take a guess: "))
+
 
         while guess != number:
 
-        if guess >= number:
-              print("You retard, too high")
-        elif guess <= number:
-              print("You retard, too low")
+
+
+
+
+
+
+                if guess > number:
+                    print("You retard, too high")
+                elif guess < number:
+                    print("You retard, too low")
+
+
+
+                guess = int(input("Try again: "))
+
+                if guess == number:
+                    print(f"Good job, {name} You guessed my number!")
+
+                    break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
