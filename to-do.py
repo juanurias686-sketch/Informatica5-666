cc15869ed0d914f971a21196a30980d2e976424a
juanurias686-sketch <juanurias686@gmail.com>
2026-09-17 15:37:16 +0000

    list to do

--- to-do.py
+++ to-do.py
@@ -8,14 +8,29 @@ def main():
         print(f"Tasks to do: {len(tasks)}")
         print(tasks)
         command = input("What do you want to do? (add, complete, exit): ")
-        if command == "add":
+
+        if command == "exit":
+                    break
+
+
+        elif command == "add":
             new_task = input ("Enter new task: ")
             tasks.append(new_task)
+            
+
+
+
 
         elif command == "complete":
-            complete = int(input("Select completed task (starting from 0): " ))
-            tasks.remove({complete})
-            print(tasks)
+            complete = input("Enter complete task: ")
+            tasks.remove(complete)
+
+
+
+
+
+
+
 
 
 
