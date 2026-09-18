c82c92b704b671caac5efc64c14272e352c6ab81
juanurias686-sketch <juanurias686@gmail.com>
2026-09-18 20:20:39 +0000

    for loops

---
+++ for-intro.py
@@ -0,0 +1,17 @@
+def main():
+
+    doctrine = ["Faith in Jesus Christ", "Repentance", "Baptize", "Receive the holy ghost", "Endure to be saved"]
+    for _ in doctrine:
+        index = 0
+        while index < len(doctrine):
+            print(doctrine[index])
+            index += 1
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
