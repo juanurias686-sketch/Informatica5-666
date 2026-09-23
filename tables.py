78d9a3828d416135834618ec3967ad735e7a5f20
juanurias686-sketch <juanurias686@gmail.com>
2026-09-23 17:30:20 +0000

    tables

---
+++ tables.py
@@ -0,0 +1,29 @@
+def main():
+
+
+    tables = [1,2,3,4,5,6,7,8,9,10]
+    number = int(input("Enter number(1-10): "))
+    for i in range(10):
+        print(i + 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
