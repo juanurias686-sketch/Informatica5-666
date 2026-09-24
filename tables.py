99d342991b69d7983134de05df46e755a6706f8d
juanurias686-sketch <juanurias686@gmail.com>
2026-09-24 15:21:51 +0000

    ttables.py

--- tables.py
+++ tables.py
@@ -1,10 +1,29 @@
 def main():
+    valid_nums = []
+    for i in range(1,11):
+        valid_nums.append(str(i))
+
+    while True:
+
+        times_table = input("Enter a number(1 to 10 or exit): ").lower().strip()
 
 
-    tables = [1,2,3,4,5,6,7,8,9,10]
-    number = int(input("Enter number(1-10): "))
-    for i in range(10):
-        print(i + 1)
+        if times_table == "exit":
+            break
+
+
+        elif times_table in valid_nums:
+            maxvalue = int(input("Enter maximum value for the times table: "))
+
+
+            print(f"Here is the {times_table} times table")
+
+
+            for x in range(1,maxvalue + 1):
+                answer = x * int(times_table)
+                print(f"{x} times {times_table} is {answer}")
+        else:
+            print("Invalid command.")
 
 
 
